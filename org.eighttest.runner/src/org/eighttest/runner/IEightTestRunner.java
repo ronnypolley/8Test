a8cc1c5adcdcd90f7ad8225c5f2e796b2d1ce1d8
ronnypolley <ronnypolley@googlemail.com>
2013-10-24 14:05:17 +0200

    added maven parent project

--- org.eighttest.runner/src/org/eighttest/runner/IEightTestRunner.java
+++ org.eighttest.runner/src/org/eighttest/runner/IEightTestRunner.java
@@ -1,5 +1,8 @@
 package org.eighttest.runner;
 
+/**
+ * The Interface IEightTestRunner used for the runner osgi-service.
+ */
 public interface IEightTestRunner {
 
 }
--- org.eighttest.runner/src/org/eighttest/runner/testrunner/EightTestRunner.java
+++ org.eighttest.runner/src/org/eighttest/runner/testrunner/EightTestRunner.java
@@ -7,35 +7,79 @@ import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eighttest.runner.IEightTestRunner;
 
 /**
- * @author rpolley
+ * The Class EightTestRunner.
  * 
+ * @author rpolley
  */
 public class EightTestRunner implements IEightTestRunner,
 		IResourceChangeListener {
 
+	/** The workspace. */
+	private IWorkspace workspace;
+
 	/**
-	 *
+	 * Instantiates a new eight test runner.
 	 */
 	public EightTestRunner() {
-		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Activates the services.
+	 */
 	public void activate() {
-		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		workspace = ResourcesPlugin.getWorkspace();
+		addListener();
+	}
+
+	/**
+	 * Deactivates the services.
+	 */
+	public void deactivate() {
+		if (workspace != null) {
+			workspace.removeResourceChangeListener(this);
+		}
+	}
+
+	/**
+	 * Adds this class as a {@link IResourceChangeListener} to the workspace.
+	 */
+	private void addListener() {
 		workspace.addResourceChangeListener(this,
 				IResourceChangeEvent.POST_CHANGE);
 	}
 
-	public void deactivate() {
-
-	}
-
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.core.resources.IResourceChangeListener#resourceChanged(org
+	 * .eclipse.core.resources.IResourceChangeEvent)
+	 */
 	@Override
 	public void resourceChanged(IResourceChangeEvent event) {
+		Job job = new Job("Fetch all Tests") {
 
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				try {
+					IJavaProject[] projects = JavaCore.create(
+							workspace.getRoot()).getJavaProjects();
+					for (IJavaProject iJavaProject : projects) {
+					}
+				} catch (JavaModelException e) {
+					e.printStackTrace();
+				}
+				return null;
+			}
+		};
 	}
-
 }
